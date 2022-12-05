ffeb3b25eb14dcada76c5ef073029b55eb16345a
mJuan0706 <114187685+mJuan0706@users.noreply.github.com>
2022-12-05 20:46:32 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const lista = [
+    "Juan",
+    24,
+    true,
+    new Date(1998, 05, 17),
+    { 
+        titulo: "El monje que vendió su Ferrari",
+        autor: "Robin S. Sharma",
+        fecha: new Date(1996, 0, 1),
+        url: "https://www.amazon.es/dp/B0062XBS32/"
+      },
+  ];
